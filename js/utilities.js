a486f80865383a6d9f3d1b55b64dd7b0619eca00
Konrad <konrad.wojtylo.9@gmail.com>
2025-06-12 23:49:23 +0200

    Added mechanics and made game more playable

--- js/utilities.js
+++ js/utilities.js
@@ -4,31 +4,38 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
 		rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
 		rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
 		rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-	)
+	);
 }
 
-function determineWinner({player, enemy, timerId}) {
-	clearTimeout(timerId)
-	document.querySelector('#textDisplay').style.display = 'flex'
+function determineWinner({ player, enemy, timerId }) {
+	clearTimeout(timerId);
+
+	document.querySelector('#textDisplay').style.display = 'flex';
+
 	if (player.health === enemy.health) {
-		document.querySelector('#textDisplay').innerHTML = 'Tie'
+		document.querySelector('#textDisplay').innerHTML = 'Remis';
 	} else if (player.health > enemy.health) {
-		document.querySelector('#textDisplay').innerHTML = 'Player 1 Wins'
+		document.querySelector('#textDisplay').innerHTML = 'Gracz nr 1 wygrał';
 	} else if (player.health < enemy.health) {
-		document.querySelector('#textDisplay').innerHTML = 'Player 2 Wins'
+		document.querySelector('#textDisplay').innerHTML = 'Gracz nr 2 wygrał';
 	}
+
+	gameOver = true;
+
+	const restartBtn = document.querySelector('#restartBtn');
+	restartBtn.style.display = 'inline-block';
 }
 
-let timer = 60
-let timerId
+let timer = 60;
+let timerId;
 function decreaseTimer() {
 	if (timer > 0) {
-		timerId = setTimeout(decreaseTimer, 1000)
-		timer--
-		document.querySelector('#timer').innerHTML = timer
+		timerId = setTimeout(decreaseTimer, 1000);
+		timer--;
+		document.querySelector('#timer').innerHTML = timer;
 	}
 
-	if(timer === 0) {
-		determineWinner({player, enemy})
+	if (timer === 0) {
+		determineWinner({ player, enemy, timerId });
 	}
 }
